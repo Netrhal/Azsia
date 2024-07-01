97d76b302d1767d414fcb992569e4ade735cc163
Netrhal <netrhal001@gmail.com>
2024-07-01 21:40:30 +0200

    Build Container

---
+++ src/main/java/com/study/azsia/AzsiaApplication.java
@@ -0,0 +1,13 @@
+package com.study.azsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzsiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzsiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/azsia/AzsiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.azsia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzsiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
