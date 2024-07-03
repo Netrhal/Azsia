1864edbb6f80fa85fb93b7b8722e1070d8c7d2ab
Netrhal <netrhal001@gmail.com>
2024-07-03 17:07:13 +0200

    Began Making Users with Address

---
+++ src/main/java/com/study/azsia/models/Address.java
@@ -0,0 +1,20 @@
+package com.study.azsia.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
